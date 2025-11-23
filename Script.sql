80d9341d6ef0d92cba17d96ad3146da9509d8340
Mohannad Mabrouk <52520054+MohannadMabrouk@users.noreply.github.com>
2025-11-23 10:01:01 +0800

    MYSQL Final Query

--- Script.sql
+++ Script.sql
@@ -1,59 +1,77 @@
-SELECT *
-FROM customer_churn_telecom_services;
+-- Created a new table with snake_case column name to copy the old table to the new table
 
-
--- PART I Churn Rate Analysis
-
-/*What is the overall churn rate?*/
-SELECT	Churn,
-		COUNT(*) AS total_customers,
-        ROUND(COUNT(*) * 100.0 / SUM(COUNT(*)) OVER(), 1) AS churn_percentage
-FROM customer_churn_telecom_services
-GROUP BY Churn;
-
-/*Which customer segments have the highest churn?*/
-
-SELECT	Contract, COUNT(Churn) AS total_churn,
-		ROUND(COUNT(Churn) * 100 / SUM(COUNT(Churn)) OVER(), 1) AS churn_contract_percentage
-FROM	customer_churn_telecom_services
-GROUP BY Contract;
--- Customers paying month-to-month leave at 55%, compared to only 21% for yearly contracts.
-
--- PART II Churn by Demographics
-
-/*Does age group affect churn?*/
-
+CREATE TABLE crypto_support_tickets_clean (
+    ticket_id               INT PRIMARY KEY,
+    ticket_date             DATE NOT NULL,
+    channel_type            VARCHAR(10) NOT NULL,         -- 'chat' / 'email'
+    agent_name              VARCHAR(100) NOT NULL,
+    created_time            TIME NOT NULL,
+    end_time                TIME NOT NULL,
+    resolution_time_min     INT NOT NULL,                 -- e.g. 76
+    first_response_time_min INT NOT NULL,                 -- e.g. 10
+    csat_score              TINYINT NOT NULL,
+    sla_met                 TINYINT(1) NOT NULL,          -- 1 = Yes, 0 = No
+    issue_severity          VARCHAR(10) NOT NULL,         -- Low / Medium / High / Urgent
+    escalated               TINYINT(1) NOT NULL,          -- 1 = Yes, 0 = No
+    cor                     VARCHAR(3) NOT NULL,          -- country code
+    user_type               VARCHAR(20) NOT NULL,         -- Retail / VIP / etc.
+    main_category           VARCHAR(50) NOT NULL,
+    sub_category            VARCHAR(100) NOT NULL
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
+INSERT INTO crypto_support_tickets_clean (
+    ticket_id,
+    ticket_date,
+    channel_type,
+    agent_name,
+    created_time,
+    end_time,
+    resolution_time_min,
+    first_response_time_min,
+    csat_score,
+    sla_met,
+    issue_severity,
+    escalated,
+    cor,
+    user_type,
+    main_category,
+    sub_category
+)
 SELECT
-    CASE
-        WHEN SeniorCitizen = 1 THEN 'Senior Citizen'
-        ELSE 'Adult'
-    END AS Age_group,
-    Churn,
-    COUNT(*) AS churn_count,
-    ROUND(COUNT(*) * 100 / SUM(COUNT(*)) OVER(PARTITION BY CASE
-                WHEN SeniorCitizen = 1 THEN 'Senior Citizen'
-                ELSE 'Adult'
-            END), 2) AS churn_rate_percentage
-FROM customer_churn_telecom_services
-GROUP BY
-    CASE
-        WHEN SeniorCitizen = 1 THEN 'Senior Citizen'
-        ELSE 'Adult'
-    END,
-    Churn
-ORDER BY Age_group, Churn;
+	ticket_id  AS ticket_id,
+    CAST(`Date` AS DATE) AS ticket_date,
+    `type` AS channel_type,
+    `agent name` AS agent_name,
+    CAST(`created time` AS TIME) AS created_time,
+    CAST(`end time` AS TIME) AS end_time,
+    CAST(REPLACE(`resolution time`, ' min', '') AS UNSIGNED) AS resolution_time_min,
+    CAST(`First Response Time (min)` AS UNSIGNED) AS first_response_time_min,
+    CAST(`csat score` AS UNSIGNED) AS csat_score,
+    CASE WHEN `SLA Met` = 'Yes' THEN 1 ELSE 0 END AS sla_met,
+    `Issue Severity` AS issue_severity,
+    CASE WHEN `Escalated` = 'Yes' THEN 1 ELSE 0 END AS escalated,
+    `COR` AS cor,
+    `User Type` AS user_type,
+    `main category` AS main_category,
+    `sub category` AS sub_category
+FROM crypto_cs_tickets;
 
+DROP TABLE crypto_cs_tickets;
 
+-- ------------------------------------------------------------------------------------------ --
+SELECT * FROM crypto_support_tickets_clean;
+-- ------------------------------------------------------------------------------------------ --
 
+-- What is the percentage of total ticket (Chat & email) received a good & bad survey?
 
+SELECT	CASE WHEN csat_score <= 3 THEN "bad" ELSE "good" END AS survey,
+		COUNT(*) AS total_tickets,
+        ROUND(COUNT(*) * 100 / (SELECT COUNT(*) FROM crypto_support_tickets_clean),2) AS survey_percentage
+FROM crypto_support_tickets_clean
+GROUP BY CASE WHEN csat_score <= 3 THEN "bad" ELSE "good" END;
 
-
-
-
-
-
-
-
+-- ------------------------------------------------------------------------------------------ --
+/* | good %67.48 | bad 32.53 |
+-- ------------------------------------------------------------------------------------------ --
 
 
 
